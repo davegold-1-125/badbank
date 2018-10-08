654735f2470e07adef9b1676b0391df2f3864c73
unknown <DG-laptop@DESKTOP-IN860RR.mit.edu>
2018-10-07 21:26:57 -0400

    HW3 local complete

--- index.js
+++ index.js
@@ -1,69 +1,184 @@
+// bash installs:
+//npm install express
+//npm install lowdb
+// BUT npm install should install all dependencies in dependencies file
+
 // setup server
 // YOUR CODE
+var express = require('express');
+var app     = express();
 
 // setup directory used to serve static files
 // YOUR CODE
+app.use(express.static('public'));
 
 // setup data store
 // YOUR CODE
+//lowdb 
+var low     = require('lowdb');
+var fs      = require('lowdb/adapters/FileSync');
+var adapter = new fs('db.json');
+var db      = low(adapter);
 
 // required data store structure
 // YOUR CODE
-/*
-{ 
-    accounts:[
-        {name        : '',
-         email       : '',
-         balance     : 0,
-         password    : '',
-         transactions: []}
-    ] 
+db.defaults(
+    { 
+        accounts:[
+            /*{name        : 'Example',
+            email       : 'ex@mple.com',
+            balance     : 0,
+            password    : 'example',
+            transactions: []}*/
+        ] 
+    }
+).write();
+
+
+//Start server--------------- added section
+app.listen(3000, function(){
+    console.log('Running on port 3000');
+});
+
+var current_user=""
+
+function log_transaction(email, trans_type, amount) {
+    //track transactions
+    var this_transaction =[Date(), trans_type, amount]
+    var account = db.get('accounts').find({ "email": email }).value();
+    account.transactions.push(this_transaction)
+    db.write()
 }
-*/
 
 app.get('/account/create/:name/:email/:password', function (req, res) {
 
     // YOUR CODE
     // Create account route
     // return success or failure string
+    var msg
+    var account
+    try {
+        //Check for duplicates
+        account = db.get('accounts').find({ "name": req.params.name }).value();
+        if (account!=null){msg = "Name exists"; console.log(msg); res.send(msg); return}
+        account = db.get('accounts').find({ "email": req.params.email }).value();
+        if (account!=null){msg = "Email exists"; console.log(msg); res.send(msg); return}
+        
+        var new_account = {
+            "name"     : req.params.name,
+            "email"    : req.params.email,
+            "balance"  : 0,
+            "password" : req.params.password,
+            "transactions": []
+        };
+        db.get('accounts').push(new_account).write();
+        
+        log_transaction(req.params.email, "Account created", 0) //track transactions
+
+        msg = "Success, account added!"
+        res.send(msg); console.log(msg)
+
+    } catch (error) {
+        msg ='Failure! error detected: '+ error
+        res.send(msg); console.log(msg)
+    }
 });
 
 app.get('/account/login/:email/:password', function (req, res) {
 
     // YOUR CODE
     // Login user - confirm credentials
-    // If success, return account object    
-    // If fail, return null
+    // If success, return account object ??why would we return this?
+    // If fail, return null ??returning error message instead
+    var msg
+    var account
+    account = db.get('accounts').find({ "email": req.params.email, "password": req.params.password }).value();
+    if (account==null){msg = "Incorrect login credentials"; console.log(msg); res.send(msg); return}
+    current_user=req.params.email
+    msg = "Login success! Logged in as " + current_user; console.log(msg); res.send(msg); return    
+});
+
+app.get('/account/deposit/:email/:amount', function (req, res) {
+    
+    // YOUR CODE
+    // Deposit amount for email
+    // return success or failure string
+    var msg
+    var account
+    
+    amount = Number(req.params.amount);
+    //errors
+    if (current_user!=req.params.email){msg = "Error: You are not logged in as " + req.params.email ; console.log(msg); res.send(msg); return}
+    if (isNaN(amount)|amount <=0){msg = "Failure: amount must be a positive number"; console.log(msg); res.send(msg); return}    
+    //manipulations
+    account = db.get('accounts').find({ "email": req.params.email}).value();
+    account.balance += amount
+    db.write()
+
+    log_transaction(req.params.email, "Deposit", amount) //track transactions
+    
+    //success
+    msg = "Deposit success! Deposited amount " + amount; console.log(msg); res.send(msg); return    
+});
+
+app.get('/account/withdraw/:email/:amount', function (req, res) {
+    
+    // YOUR CODE
+    // Withdraw amount for email
+    // return success or failure string
+    var msg
+    var account
+    
+    amount = Number(req.params.amount);
+    //errors
+    if (current_user!=req.params.email){msg = "Error: You are not logged in as " + req.params.email ; console.log(msg); res.send(msg); return}
+    if (isNaN(amount)|amount <=0){msg = "Failure: amount must be a positive number"; console.log(msg); res.send(msg); return}    
+    //manipulations
+    account = db.get('accounts').find({ "email": req.params.email}).value();
+    account.balance -= amount
+    db.write()
+
+    log_transaction(req.params.email, "Withdrawal", amount) //track transactions
+
+    //success
+    msg = "Withdraw success! Withdraw amount " + amount; console.log(msg); res.send(msg); return    
+});
+
+app.get('/account/transactions/:email', function (req, res) {
+    
+    // YOUR CODE
+    // Return all transactions for account
+    //time, type, ammount
+    var msg
+    var account
+    if (current_user!=req.params.email){msg = "Error: You are not logged in as " + req.params.email ; console.log(msg); res.send(msg); return}
+    account = db.get('accounts').find({ "email": req.params.email}).value();
+    transactions = account.transactions
+    msg = "Transactions for " + req.params.email +" are: "+ transactions; console.log(msg); res.send(msg); return
+
+
 });
 
 app.get('/account/get/:email', function (req, res) {
 
     // YOUR CODE
     // Return account based on email
-});
+    var msg
+    var account
+    
+    //errors
+    if (current_user!=req.params.email){msg = "Error: You are not logged in as " + req.params.email ; console.log(msg); res.send(msg); return}
 
-app.get('/account/deposit/:email/:amount', function (req, res) {
+    account = db.get('accounts').find({ "email": req.params.email}).value();
+    balance = account.balance
 
-    // YOUR CODE
-    // Deposit amount for email
-    // return success or failure string
-});
-
-app.get('/account/withdraw/:email/:amount', function (req, res) {
-
-    // YOUR CODE
-    // Withdraw amount for email
-    // return success or failure string
-});
-
-app.get('/account/transactions/:email', function (req, res) {
-
-    // YOUR CODE
-    // Return all transactions for account
+    //success
+    msg = "Current balance for "+req.params.email+" is: " + balance; console.log(msg); res.send(msg); return    
 });
 
 app.get('/account/all', function (req, res) {
-
     // YOUR CODE
     // Return data for all accounts
+    res.send(db.get('accounts').value());
+    console.log('All data sent')
 });
--- public/api/api.js
+++ public/api/api.js
@@ -3,7 +3,26 @@ function create() {
     // -------------------------------------
     //  YOUR CODE
     //  Create user account on server
-    // -------------------------------------    
+    // -------------------------------------
+    var name  = document.getElementById("inputName").value;
+    var email  = document.getElementById("inputEmail").value;
+    var password  = document.getElementById("inputPassword").value;
+    
+    var url = '/account/create/' + name +'/'+ email+'/'+ password;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });
+    console.log('Creating Account: '+ name +', '+ email+', '+ "Secret Password")
 }
 
 function login() {
@@ -11,6 +30,24 @@ function login() {
     //  YOUR CODE
     //  Confirm credentials on server
     // -------------------------------------
+    console.log('Logging In')    
+    var email  = document.getElementById("inputEmail").value;
+    var password  = document.getElementById("inputPassword").value;
+    
+    var url = '/account/login/' + email+'/'+ password;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });
 }
 
 function deposit() {
@@ -18,6 +55,24 @@ function deposit() {
     //  YOUR CODE
     //  Deposit funds user funds on server
     // -------------------------------------
+    console.log('Depositing')    
+    var email  = document.getElementById("inputEmail").value;
+    var amount  = document.getElementById("inputAmount").value;
+    
+    var url = '/account/deposit/' + email+'/'+ amount;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });
 }
 
 function withdraw() {
@@ -25,6 +80,24 @@ function withdraw() {
     //  YOUR CODE
     //  Withdraw funds user funds on server
     // -------------------------------------
+    console.log('Withdrawing')    
+    var email  = document.getElementById("inputEmail").value;
+    var amount  = document.getElementById("inputAmount").value;
+    
+    var url = '/account/withdraw/' + email+'/'+ amount;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });
 }
 
 function transactions() {
@@ -32,6 +105,24 @@ function transactions() {
     //  YOUR CODE
     //  Get all user transactions
     // -------------------------------------
+    console.log('Showing Transactions')
+    var email  = document.getElementById("inputEmail").value;
+    
+    var url = '/account/transactions/' + email;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });    
+
 }
 
 function balance() {
@@ -39,6 +130,23 @@ function balance() {
     //  YOUR CODE
     //  Get user balance
     // -------------------------------------
+    console.log('Getting Balance')    
+    var email  = document.getElementById("inputEmail").value;
+    
+    var url = '/account/get/' + email;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.text);
+            }
+
+        });
 }
 
 function allData() {
@@ -46,5 +154,21 @@ function allData() {
     //  YOUR CODE
     //  Get all data
     // -------------------------------------
+    console.log('Showing All Data')    
+    var url = '/account/all';
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                output.innerHTML = JSON.stringify(res.body);
+            }
+
+        });
+
 }
 
